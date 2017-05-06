a8f9891ffdf7af3d04c72c5128e170e8d04bd87f
Amir Ismagilov <progamailism@gmail.com>
2017-05-06 15:11:24 +0500

    Update script.js

--- script.js
+++ script.js
@@ -67,6 +67,14 @@ function wallGet(owner_id)
     });
   FB.AppEvents.logPageView();
 
+  (function(d, s, id){
+     var js, fjs = d.getElementsByTagName(s)[0];
+     if (d.getElementById(id)) {return;}
+     js = d.createElement(s); js.id = id;
+     js.src = "//connect.facebook.net/en_US/sdk.js";
+     fjs.parentNode.insertBefore(js, fjs);
+   }(document, 'script', 'facebook-jssdk'));
+
   FB.api(
     "/search",
     {
